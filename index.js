e61a147a79e5a30c62ef72ff0c60cf28ed227a9f
Alberto Alfaro <aalfaro88@gpofinalpa.com>
2023-07-10 14:38:08 -0600

    Lab Done

--- index.js
+++ index.js
@@ -16,8 +16,59 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Grilled Portobello Mushroom Pizzas",
+      level: "Easy Peasy",
+      ingredients: ["Pepperoni","Chopped Red Onion", "Diced bell Pepper","Capers","Sliced prosciutto"],
+      cuisine: "Italian",
+      dishType: "snack",
+      image: "https://www.simplyrecipes.com/thmb/5LTh4yncb14F_E8aTkkIT_v8rjE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Grilled-Mushroom-Pizza-LEAD-12-952eead397b74cef85db0564b49ff202.jpg",
+      duration: 25,
+      creator: "Simply Recipes",
+      created: Date.now()
+    });
   })
+
+  .then(recipe => {
+    console.log(`${recipe.title} ready!`);
+  })
+  
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+
+  .then(recipes => {
+    recipes.forEach(recipe => {
+      console.log(`Added recipe: ${recipe.title}`);
+    });
+  })
+
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
+  })
+
+  .then(() => {
+    console.log("Time Succesfully Updated!");
+  })
+
+  .then(() => {
+    return Recipe.deleteOne ({
+      title: "Carrot Cake"
+    })
+  })
+
+  .then(() => {
+    console.log("Succesfully Deleted Carrot Cake!");
+  })
+
+  .then(() => {
+    console.log("Connection Closed");
+    mongoose.connection.close();
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
